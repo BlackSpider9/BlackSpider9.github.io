a0a45a90583b0d1cd034e30d9545f890bb59a85e
高鑫 <448607070@qq.com>
2020-01-06 14:35:51 +0800

    提交测试页面

---
+++ js/component/head.js
@@ -0,0 +1,19 @@
+/*
+* css
+* */
+document.write('<link href="css/bootstrap/bootstrap.css" rel="stylesheet">');
+// document.write('<link href="css/bootstrap/bootstrap-theme.css" rel="stylesheet">');
+
+document.write('<link href="css/bootstrap-table/bootstrap-table.css" rel="stylesheet">');
+
+/*
+* js
+* */
+document.write('<script src="js/html5shiv.js"></script>');
+document.write('<script src="js/respond.min.js"></script>');
+document.write('<script src="js/jquery/jquery-1.11.1.js"></script>');
+document.write('<script src="js/bootstrap/bootstrap.js"></script>');
+document.write('<script src="js/bootstrap-table/bootstrap-table.js"></script>');
+document.write('<script src="js/bootstrap-table/bootstrap-table-zh-CN.js"></script>');
+
+document.write('<link href="css/custom/style.css" rel="stylesheet">');
